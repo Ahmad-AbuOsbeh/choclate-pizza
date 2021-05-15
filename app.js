e90c617b2c82cf00992cb479889cffaad4a7e01f
Ahmad Abu Osbeh <ahmadabuosbeh20@gmail.com>
2021-05-15 19:32:06 +0300

    edited css link remove /

---
+++ app.js
@@ -0,0 +1,127 @@
+// var var1 =[]
+// console.log(var1);
+// let let1= 'hello from let1';
+// console.log(let1);
+
+//  let userName='samer';
+//  console.log(userName);
+//             userName='ahmad'
+//             console.log(userName);
+
+// const url='https://github.com/LTUC/amman-201d20';
+// console.log(url);        
+
+//    url='samer';   
+//    console.log(url);    
+
+// let age= prompt('what is  your age ?')
+// console.log(age);
+// console.log(typeof(age));
+// let ageAsNumber= parseInt(age);
+// console.log(ageAsNumber);
+// console.log(typeof(ageAsNumber));
+
+// let x= 0;
+
+// while (x<6) {
+//     console.log(x);
+
+//     if (x==3) {
+//        x=5;
+//         continue;
+
+//     }
+//     x++
+// }
+// let info=['ahmad', 22,1.86,['pizza','mansaf','burger']];
+
+// console.log(info);
+// let x=5;
+// let y=7;
+
+// x=y
+// console.log(x);
+// x=10
+// console.log(x);
+
+// console.log(y);
+
+
+
+// if(58){
+
+//     var arr1=[1,2,3,4,5];
+//     console.log('hello from if');
+
+//     if (5) {
+//         let arr1=1000
+//     }
+
+// }
+
+// // var arr1=5;
+// let arr2=[5,6,7,8,9];
+// console.log('arr1 before',arr1);
+// console.log('arr2 before',arr2);
+// // arr1=arr2;
+// arr1=arr2
+// // arr1.push(10);
+// console.log('arr1 after',arr1);
+// console.log('arr2 after',arr2);
+// let x;
+// console.log(x);
+// let x=5;
+// let x=5;
+// let y=1000;
+// let z=10;
+// function Ahmad(w,z) {
+//     // x='hello from the function'
+//     // // console.log('hello from Ahmad');
+//     // y='y';
+//      z=x+y;
+//     return (y)
+// }
+// Ahmad();
+
+// let saed= Ahmad()
+// console.log('sead',saed);
+// console.log('log for the function calling',Ahmad());
+// console.log('log for x',x);
+
+// Write your code here
+// let sumResult;
+// function sum(a, b) {
+//     //   eslint-disable-line 
+//     let sum = a + b;
+//     sumResult=[sum, `The sum of ${a} and ${b} is ${sum}.`]
+//     return [sum, `The sum of ${a} and ${b} is ${sum}.`]
+// }
+
+// function multiply(a, b) {
+//     //eslint-disable-line 
+//     let multiply = a * b;
+//     return [multiply, `The product of ${a} and ${b} is ${multiply}.`]
+// }
+// let resultArray;
+// function sumAndMultiply(a, b, c) {
+//     //eslint-disable-line
+//     d = sum(a, b);
+//     f = sum(d[0], c);
+//     // console.log(f[0]);
+
+//     h = multiply(a, b);
+//     i = multiply(h[0], c);
+//     // console.log(i[0]);  
+//     // return [a,b,c,d,f,h,i]
+
+//  resultArray=[f[0], i[0], `${a} and ${b} and ${c} sum to ${f[0]}.`, `The product of ${a} and ${b} and ${c} is ${i[0]}.`]
+//     return [f[0], i[0], `${a} and ${b} and ${c} sum to ${f[0]}.`, `The product of ${a} and ${b} and ${c} is ${i[0]}.`]
+// }
+
+
+// sumAndMultiply(1, 2, 3)
+// console.log('d',d);
+// console.log(sumAndMultiply(1, 2, 3));
+// console.log('resultArray',resultArray);
+
+console.log(Math.random()); 
